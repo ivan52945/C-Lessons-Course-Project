f8b8e699ab155911f60ba905850c465f8cad81c9
ivan52945 <ivan52945@gmail.com>
2024-04-15 22:24:27 +0300

    refactor: type temperature record is replaced to special file for reusing

---
+++ headers/t_record_type.h
@@ -0,0 +1,10 @@
+#pragma once
+
+typedef struct {
+    unsigned short year;
+    unsigned char mounth;
+    unsigned char day;
+    unsigned char hour;
+    unsigned char minute;
+    char temp;
+} t_record;
