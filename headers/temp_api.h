1f364302addbb1d06972ce9b2e99293b1f5576e5
ivan52945 <ivan52945@gmail.com>
2024-04-16 17:55:55 +0300

    feat: api for statistics

--- headers/temp_api.h
+++ headers/temp_api.h
@@ -1,14 +1,14 @@
 #pragma once
 #include "t_record_type.h"
 
-char t_average_mount(unsigned long n, t_record a [], unsigned short year, unsigned char mount);
+char t_average_mount(unsigned long n_rec, t_record a [], int month_i, t_record* months []);
 
-char t_min_mounth(unsigned long n, t_record a [], unsigned short year, unsigned char mount);
+char t_min_mounth(unsigned long n_rec, t_record a [], int month_i, t_record* months []);
 
-char t_max_mounth(unsigned long n, t_record a [], unsigned short year, unsigned char mount);
+char t_max_mounth(unsigned long n_rec, t_record a [], int month_i, t_record* months []);
 
-char t_average_year(unsigned long n, t_record a [], unsigned short year);
+char t_average_year(unsigned long n_rec, t_record a []);
 
-char t_min_year(unsigned long n, t_record a [], unsigned short year);
+char t_min_year(unsigned long n_rec, t_record a []);
 
-char t_max_year(unsigned long n, t_record a [], unsigned short year);
+char t_max_year(unsigned long n_rec, t_record a []);
